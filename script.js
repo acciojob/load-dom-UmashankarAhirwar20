7e67000134eba1d9ce34044e2f68409dc706e05c
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2026-01-20 12:01:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-let body = document.querySelector("body");
-
-body.append("DOM load success")
+document.addEventListener("DOMContentLoaded", ()=>{
+	document.body.innerText = "DOM load success";
+})
